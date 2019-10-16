922fa427fa7db1c944ff6b6648bc9961c66900b7
zakfarrugia <56064000+zakfarrugia@users.noreply.github.com>
2019-10-16 09:28:45 +0200

    triggers and methods

---
+++ BlockBreakerSWD42C/Assets/Scripts/LoseCollider.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoseCollider : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D myCollider)
+    {
+        SceneManager.LoadScene("GameOver");
+
+        print("Game Over");
+        print(myCollider.gameObject.name);
+    }
+
+    
+   
+
+}
